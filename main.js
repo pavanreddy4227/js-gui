47a3e52b3c33ba06586e20c48498867c8bfc71ea
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2019-09-30 13:09:01 -0500

    uoyitr

--- main.js
+++ main.js
@@ -3,12 +3,13 @@
 const updateWithAdd = async (event) => {
   document.querySelector('#result').innerHTML = ''
   if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
-    const regex = /[^a-zA-Z_]/g
+  
     const i = parseInt(document.querySelector('#firstNumber').value)
     const j = parseInt(document.querySelector('#secondNumber').value)
     const k = parseInt(document.querySelector('#thirdNumber').value)
     const ans = i*j*k;
-    document.querySelector('#result').innerHTML = ans
+    document.querySelector('#result').innerHTML = "Volume is:"+ans
+  }
   
 }
 
